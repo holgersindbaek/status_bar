36247a0c7416eb8e8349e61e3955bc93710ff931
holgersindbaek <holgersindbaek@gmail.com>
2013-09-30 08:27:28 +0200

    Changed images a bit

--- lib/StatusBar.rb
+++ lib/StatusBar.rb
@@ -8,6 +8,17 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
+lib_dir_path = File.dirname(File.expand_path(__FILE__))
+resource_dir = File.expand_path(File.join(lib_dir_path, '..', 'resources'))
+
+p __FILE__
+p File.join(File.dirname(__FILE__), 'resources')
+p File.join('../resources')
+
+p File.join(lib_dir_path, "project/StatusBar.rb")
+p File.join(lib_dir_path, "project/StatusBarHelper.rb")
+p "resource_dir: #{resource_dir}"
+
 require 'motion-require'
 
 Motion::Require.all(Dir.glob(File.expand_path('../StatusBar/**/*.rb', __FILE__)))
