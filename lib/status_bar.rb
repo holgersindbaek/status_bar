b6029272a686a1d2661c42eeedd74dd52b4b264b
holgersindbaek <holgersindbaek@gmail.com>
2013-10-29 21:16:11 +0100

    added pods

--- lib/status_bar.rb
+++ lib/status_bar.rb
@@ -3,15 +3,14 @@ unless defined?(Motion::Project::Config)
 end
 
 lib_dir_path = File.dirname(File.expand_path(__FILE__))
-resource_dir = File.expand_path(File.join(lib_dir_path, 'resources'))
-  
-puts resource_dir
+# resource_dir = File.expand_path(File.join(lib_dir_path, '..', 'resources'))
 
 Motion::Project::App.setup do |app|
   # app.files_dependencies File.join(lib_dir_path, "project/StatusBar.rb") => File.join(lib_dir_path, "project/StatusBarHelper.rb")
   app.files.unshift Dir.glob(File.join(lib_dir_path, "project/**/*.rb"))
-  app.resources_dirs += [resource_dir]
+  app.resources_dirs << File.join(File.dirname(__FILE__), 'resources')
 
+  app.pods ||= Motion::Project::CocoaPods.new(app)
   app.pods do
     pod 'UIImage+RTTint', '~> 1.0.0'
   end
