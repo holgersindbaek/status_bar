8899af54e0ef3184f25821d08de061849a09af54
holgersindbaek <holgersindbaek@gmail.com>
2013-10-31 20:13:46 +0100

    saving

--- lib/status_bar.rb
+++ lib/status_bar.rb
@@ -15,6 +15,21 @@ lib_dir_path = File.dirname(File.expand_path(__FILE__))
 
 Motion::Project::App.setup do |app|
   # app.files_dependencies File.join(lib_dir_path, "project/StatusBar.rb") => File.join(lib_dir_path, "project/StatusBarHelper.rb")
+  teacup_insert_point = nil
+  app.files.each_index do |index|
+    file = app.files[index]
+    if file =~ /teacup/
+      teacup_insert_point = index + 1
+    elsif teacup_insert_point
+      # found one, so stop looking
+      break
+    end
+  end
+
+  Dir.glob(File.join(File.dirname(__FILE__), 'sweettea/**/*.rb')).reverse.each do |file|
+    app.files.insert(teacup_insert_point, file)
+  end
+  
   app.files.unshift Dir.glob(File.join(lib_dir_path, "project/**/*.rb"))
   app.resources_dirs << File.join(File.dirname(__FILE__), 'resources')
 end
